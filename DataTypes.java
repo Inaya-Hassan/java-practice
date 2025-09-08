0d97237cee133fdc94db50114ffa943c57cd82d1
Inaya <inayahassan0@gmail.com>
2025-09-08 10:36:59 +0500

    Removed error

--- DataTypes.java
+++ DataTypes.java
@@ -1,16 +1,21 @@
 public class DataTypes{
 	public static void main(String a[]){
 	 System.out.println("Basic Data Types in java:");
-	 System.out.println("       *******************");
-	 System.out.println("       *                 *");
-	 System.out.println("       *                 *");
-	 System.out.println("       *                 *");
-	 System.out.println("       *                 *");
-	 System.out.println("       *******************");
-	 System.out.printf("\n Value in int %d ,i");
-	 //System.out.printf("\n Value in  %d ,i");
-	//System.out.printf(" %50s","Inaya");
-System.out.printf("%50s", "Inaya");
+
+
+	 System.out.println("*******************");
+	 System.out.println("*                 *");
+	 System.out.println("*                 *");
+	 System.out.println("*                 *");
+	 System.out.println("*                 *");
+	 System.out.println("*******************");
+	//int i = 100;
+	// System.out.printf("Value in int %d ,i");
+	 //System.out.printf("\n Value in  %d", i);
+	//System.out.printf(" %-50s","Inaya");
+	   int i = 100;
+        System.out.printf("Value in int %d\n", i);
+	System.out.printf("%50s", "Inaya");
 
 
 	
--- Demo.java
+++ Demo.java
@@ -1,20 +1,19 @@
 public class Demo{
-	public static void main(String [] args){
-		Rectangle r;
-		Circle c;
+	public static void main(String[] args){
+	Rectangle r;
+	Circle c;
 
 	r = new Rectangle(); //Constructor
 	c = new Circle();
         r.height = 200;
+        r.width = 30;
         System.out.println("Height in rectangle r updated: " + r.height);
 
         r.width = 30;
         double area = r.calArea();
         System.out.println("Area of Rectangle: " + area);
 
-        
-        System.out.println("This is circle class");
-        System.out.println("Value in Rectangle: " + r); 
+        System.out.println("Value in Rectangle: " + r); //print ref
         System.out.println("Value in Circle: " + c);    
     }
 }
